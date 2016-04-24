11f3512014623f35c3aa587b66b10c8b765ab073
Rafał Gawlik <rafal.maly@gmail.com>
2016-04-24 21:32:53 +0200

    register alt-integration

--- app/react/index.js
+++ app/react/index.js
@@ -1,4 +1,7 @@
-import RWR from 'react-webpack-rails';
+import RWR, {integrationsManager} from 'react-webpack-rails';
+import altIntegration from './flux/alt-integration';
+
+integrationsManager.register('alt-store', altIntegration);
 RWR.run();
 
 import Counter from './components/counter';
